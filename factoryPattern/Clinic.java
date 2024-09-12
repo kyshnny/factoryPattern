7ee10c5d0fa073e5eaaa6918d8a2cd8f0737c352
kyshnny <4ushaniamae@gmail.com>
2024-09-12 23:36:32 +0800

    Update Clinic.java
    - Added Exit selection to break the loop if user choose Exit.
    - Added while loop to make prompt infinite.
    - Added additional display info, if cat is selected (display no. of lives), if dog (display dog breed).

--- factoryPattern/Clinic.java
+++ factoryPattern/Clinic.java
@@ -7,26 +7,41 @@ public class Clinic {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("[1] Dog");
-        System.out.println("[2] Cat");
-        System.out.print("\nChoose your pet number: ");
-        Integer choice = input.nextInt();
-
         PetRecord petFile = new PetRecord();
         Pet pet;
 
+    while (true){
+        System.out.println("[1] Dog");
+        System.out.println("[2] Cat");
+        System.out.println("[3] Exit");
+        System.out.print("\nChoose your pet number or Exit: ");
+        Integer choice = input.nextInt();
+
+        if (choice == 3){
+            System.out.println("Exiting...");
+            break;
+        }
+                
         switch(choice){
             case 1: pet = new Dog();
                 petFile.setPetId("D01");
                 petFile.setPetName("Bantay");
                 petFile.setPet(pet);
                 ((Dog) pet).setBreed("German Shepperd");
+                System.out.println("Dog Breed: " + ((Dog) pet).getBreed());
                 break;
+                
             case 2: pet = new Cat();
                 petFile.setPetId("C01");
                 petFile.setPetName("Muning");
                 petFile.setPet(pet);
                 ((Cat) pet).setNoOfLives(9);
+                System.out.println("Number of Lives: " + ((Cat) pet).getNoOfLives());
+                break;
+
+            default:
+                System.out.println("Invalid Choice! Select again [1], [2], or [3]");
+                continue;
         }
 
         System.out.println("Pet id is " + petFile.getPetId());
@@ -35,5 +50,6 @@ public class Clinic {
         System.out.println("Communication sound: "+ petFile.getPet().makeSound());
         System.out.println("Play mode: " + petFile.getPet().play());
 
+        }
     }
 }
